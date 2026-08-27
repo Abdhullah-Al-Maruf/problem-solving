3e9fe932b4001aa8c87a254b84de881aba7c2e3d
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-08-27 16:38:03 +0600

    even_Number

---
+++ B_Even_Numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // write your code here
+
+
+    int n;
+    scanf("%d",&n);
+    for (int i =1; i <=n; i++)
+    {
+    if (i%2==0)
+    {
+    printf("%d\n",i);
+    }
+    
+    }
+    
+    return 0;
+}
